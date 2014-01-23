0afae6fd55570b923e2d75014c6f366e413c462b
Xavier Dutoit <xavier@tttp.eu>
2014-01-23 11:38:52 +0100

    fix wrong join on website table

--- api/v3/Candidate.php
+++ api/v3/Candidate.php
@@ -1,15 +1,16 @@
 <?php 
 function civicrm_api3_candidate_get ($params) {
+
   $sqlParam = array();
   $select ="c.id as id, first_name, last_name, email, civicrm_email.id as email_id, civicrm_value_ep_1.id as candidate_id, country_3 as country, position_2 as position, party_5 as party, website.url as website, facebook.url as facebook, twitter.url as twitter, image_URL";
   $join="LEFT JOIN civicrm_value_ep_1 ON civicrm_value_ep_1.entity_id = c.id ";
-  $join .= "LEFT JOIN civicrm_email ON c.id = contact_id AND is_primary=1 ";
+  $join .= "LEFT JOIN civicrm_email ON c.id = civicrm_email.contact_id AND is_primary=1 ";
   $join .= "LEFT JOIN civicrm_website as website ON website.contact_id=c.id AND website.website_type_id=1 ";
-  $join .= "LEFT JOIN civicrm_website as facebook ON facebook.contact_id=c.id AND facebook.website_type_id=2 ";
-  $join .= "LEFT JOIN civicrm_website as twitter ON facebook.contact_id=c.id AND twitter.website_type_id=3 ";
+  $join .= "LEFT JOIN civicrm_website as facebook ON facebook.contact_id=c.id AND facebook.website_type_id=3 ";
+  $join .= "LEFT JOIN civicrm_website as twitter ON twitter.contact_id=c.id AND twitter.website_type_id=4 ";
   if (array_key_exists ("group",$params)) {
     $where = "agroup.group_id = %1 ";
-    $join .= " JOIN civicrm_group_contact as agroup ON agroup.contact_id = c.id ";
+    $join = " JOIN civicrm_group_contact as agroup ON agroup.contact_id = c.id " . $join;
     $sqlParam =  array(1 => array((int) $params["group"], 'Integer'));
   } else {
     $params["group"] = null;
@@ -23,7 +24,6 @@ function civicrm_api3_candidate_get ($params) {
 
 
   $sql = "SELECT $select FROM civicrm_contact as c $join WHERE $where";
-
   $dao = CRM_Core_DAO::executeQuery($sql, $sqlParam); 
 
   $values = array(); 
