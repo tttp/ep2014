98bc3b03ebbeef4a95339f299c067b447404426c
Xavier Dutoit <xavier@tttp.eu>
2014-03-26 14:14:47 +0100

    add api.contact.fix to update the display and sort_name based on the organisation name

--- api/v3/Contact/Fix.php
+++ api/v3/Contact/Fix.php
@@ -1,7 +1,7 @@
 <?php 
 function civicrm_api3_contact_fix ($params) {
   $value= array();
-  $sql="update civicrm_contact set display_name = organization_name, sort_name = organization_name where contact_type = "Organization" and organization_name <> display_name;";
+  $sql="update civicrm_contact set display_name = organization_name, sort_name = organization_name where contact_type = 'Organization' and organization_name <> display_name;";
   $dao = CRM_Core_DAO::executeQuery($sql); 
   return civicrm_api3_create_success($values, $params, NULL, NULL, $dao); 
 };
