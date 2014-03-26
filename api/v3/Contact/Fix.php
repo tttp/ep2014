d1fe4331227d8ce67e9e77c8b1af0370a40ed78f
Xavier Dutoit <xavier@tttp.eu>
2014-03-26 14:06:30 +0100

    missing <?php

--- api/v3/Contact/Fix.php
+++ api/v3/Contact/Fix.php
@@ -1,3 +1,4 @@
+<?php 
 function civicrm_api3_contact_fix ($params) {
   $value= array();
   $sql="update civicrm_contact set display_name = organization_name, sort_name = organization_name where contact_type = "Organization" and organization_name <> display_name;";
