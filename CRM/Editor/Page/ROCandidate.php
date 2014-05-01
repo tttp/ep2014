1e48362d71fc6b9d7d4693bd7e9a5bcff1b4be5c
Xavier Dutoit <xavier@tttp.eu>
2014-05-01 16:50:57 +0200

    add filter on country

--- CRM/Editor/Page/ROCandidate.php
+++ CRM/Editor/Page/ROCandidate.php
@@ -9,7 +9,21 @@ class CRM_Editor_Page_ROCandidate extends CRM_Core_Page {
 
     $datediff = strtotime("2014-05-22") - time();
     $days= floor($datediff/(60*60*24));
-    CRM_Utils_System::setTitle (" known candidates. $days days to go.");
+    $q = explode ("/",$_GET["q"]);
+    if (count($q)>3) {
+      $iso=strtoupper($q[3]);
+      if (strlen($iso) != 2) {
+        die ("not a country, url: /civicrm/candidate/view/fr");
+      }
+      $country=civicrm_api3("country","getsingle",array("iso_code" =>$iso));
+      $candidates = civicrm_api3("Candidate","get",array("country"=>$country["id"]));
+      $filter = "in ". $country["name"];
+    } else {
+      $filter = "";
+      $candidates = civicrm_api3("Candidate","get");
+    }
+    $this->assign("candidates", json_encode($candidates["values"]));
+    CRM_Utils_System::setTitle ($candidates["count"]." known candidates $filter. $days days to go.");
     parent::run();
   }
 }
