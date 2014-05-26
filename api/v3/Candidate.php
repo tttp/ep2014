f72297728a3e3c9d757d257c33c8ce48d3084263
Xavier Dutoit <xavier@tttp.eu>
2014-05-26 19:08:14 +0200

    trying to fix filter on candidates

--- api/v3/Candidate.php
+++ api/v3/Candidate.php
@@ -132,7 +132,7 @@ function civicrm_api3_candidate_get ($params) {
     $where .= " OR contact_sub_type like '%mep%'";
   }
   if (array_key_exists ("elected",$params)) {
-     $where .= " AND ep2014_30=1";
+     $where .= " AND ep2014_30 like '%1%'";
   }
   if (array_key_exists ("country",$params)) {
     $where .= " AND civicrm_value_ep_1.country_3 = %1";
